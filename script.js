cc0b78b4f084b59ed1e0f8aa1bf82dc0f7e46848
ianwalya <155696225+hotbite@users.noreply.github.com>
2025-03-04 12:43:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+import data from './data.js'
+
+var bc = document.getElementById('b2-cont');
+
+for(let i = 0; i<data.length; i++){
+    var div = document.createElement("div");
+    var h3 = document.createElement("h3");
+    var img = document.createElement("img");
+    div.appendChild(img);
+    img.src = "/icons/url.svg";
+    div.appendChild(h3);
+    h3.innerText = data[i];
+    bc.appendChild(div);
+    div.onclick = function(e){
+        var d = this.innerText;
+        var dd = d.replaceAll('projects','');
+        document.location.href = '/projects/'+dd;
+    }
+}
